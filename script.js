6b8eaac3f75f0e1bb25df1e690c06976436ac2d8
rhit-kimmelog <143840211+rhit-kimmelog@users.noreply.github.com>
2026-09-23 23:34:57 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let navbar = "<a href='index.html' id='main' class='link selected'>Main Page</a> <a href='resume.html' id='resume' class='link blue'>Resume</a> <a href='portfolio.html' id='portfolio' class='link blue'>Portfolio</a>";
+
+let footer = "<a href='https://app.joinhandshake.com/profiles/owenkimmel'>Handshake Profile</a>";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // https://befused.com/javascript/get-filename-url/
+    let name = window.location.pathname.substring(window.location.pathname.lastIndexOf('/')+1);
+
+    nav = document.getElementById("navbar");
+
+    if (name == "index.html") {
+        nav.innerHTML = navbar;
+    }
+
+    if (name == "resume.html") {
+        nav.innerHTML = navbar;
+        document.getElementById("main").classList.remove("selected")
+
+        document.getElementById("resume").classList.add("selected")
+
+    }
+
+    if (name == "portfolio.html") {
+        nav.innerHTML = navbar;
+        document.getElementById("main").classList.remove("selected")
+
+        document.getElementById("portfolio").classList.add("selected")
+    }
+
+    document.getElementById("footer").innerHTML = footer;
+})
